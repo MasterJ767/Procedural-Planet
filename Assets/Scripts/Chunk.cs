d84a686cb066745bbb8a5593280a78a4dcf9bc5c
MasterJ767 <jon.arundel@hotmail.com>
2023-04-04 14:19:08 +0100

    Create first chunk with world warp effect

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class Chunk : MonoBehaviour
+{
+    private void Start()
+    {
+        Render();
+    }
+
+    public void Render()
+    {
+        MeshFilter meshFilter = gameObject.GetComponent<MeshFilter>();
+        MeshRenderer meshRenderer = gameObject.GetComponent<MeshRenderer>();
+
+        List<Vector3> vertices = new List<Vector3>();
+        List<Vector3> normals = new List<Vector3>();
+        List<int> triangles = new List<int>();
+        int vertexIndex = 0;
+
+        for (int x = 0; x < Config.ChunkWidth; ++x)
+        {
+            for (int z = 0; z < Config.ChunkWidth; ++z)
+            {
+                vertices.Add(new Vector3(x * Config.Scale, 0, z * Config.Scale));
+                normals.Add(Vector3.up);
+                
+                if (x < Config.ChunkWidth - 1 && z < Config.ChunkWidth - 1) 
+                {
+                    triangles.Add(vertexIndex);
+                    triangles.Add(vertexIndex + 1);
+                    triangles.Add(vertexIndex + Config.ChunkWidth);
+                    triangles.Add(vertexIndex + Config.ChunkWidth);
+                    triangles.Add(vertexIndex + 1);
+                    triangles.Add(vertexIndex + Config.ChunkWidth + 1);
+                }
+
+                vertexIndex++;
+            }
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.SetVertices(vertices.ToArray());
+        mesh.SetTriangles(triangles.ToArray(), 0);
+        mesh.SetNormals(normals.ToArray());
+        mesh.Optimize();
+
+        meshFilter.sharedMesh = mesh;
+    }
+}
---
+++ Assets/Scripts/Config.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Config : MonoBehaviour
+{
+    public static readonly float Scale = 0.5f; 
+    public static readonly int ChunkWidth = 21;
+    public static readonly int ChunkHeight = 256;
+    public static readonly int WorldWidthInChunks = 5;
+
+    public static int WorldWidth => WorldWidthInChunks * ChunkWidth;
+    public static int MinimunTerrainHeight => ChunkHeight / 6;
+    public static int SeaLevel => ChunkHeight / 4;
+}
